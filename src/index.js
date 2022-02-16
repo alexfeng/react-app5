37edc582c142ea30605feba6bd3aa6925a1add36
冯向博 <fengxiangbo@qtone.cn>
2022-02-16 11:15:59 +0800

    1. 设置全局样式 2. 说明样式命名规范

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
 
+// 全局样式
+import '@/common/stylus/frame.styl'
+
 ReactDOM.render(<App />, document.getElementById("root"))
