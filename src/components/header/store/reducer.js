2aae1bb6e231b371c861175e0d450b4ccb6c2de0
冯向博 <fengxiangbo@qtone.cn>
2022-02-17 11:30:44 +0800

    使用 immutable

--- src/components/header/store/reducer.js
+++ src/components/header/store/reducer.js
@@ -1,17 +1,22 @@
 import * as constants from './constants'
-
+import { fromJS } from 'immutable'
 // 初始默认的 state
-const defaultState = {
+const defaultState = fromJS({
   myHeaderData: null
+})
+
+const getData = (state, action) => {
+  return state.set('myHeaderData', action.data)
 }
 
 const reducer = (state = defaultState, action) => {
   // 由于 state 是引用类型，不能直接修改，否则是检测不到 state 发生变化的。因此需要先复制一份进行修改，然后再返回新的state
-  let newState = Object.assign({}, state)
+  // let newState = Object.assign({}, state)
   switch (action.type) {
     case constants.SET_DATA:
-      newState.myHeaderData = action.data
-      return newState
+      // newState.myHeaderData = action.data
+      // return newState
+      return getData(state, action)
     default:
       return state
   }
--- src/pages/login/store/reducer.js
+++ src/pages/login/store/reducer.js
@@ -1,17 +1,22 @@
 import * as constants from './constants'
-
+import { fromJS } from 'immutable'
 // 初始默认的 state
-const defaultState = {
+const defaultState = fromJS({
   myLoginData: null
+})
+
+const getData = (state, action) => {
+  return state.set('myLoginData', action.data)
 }
 
 const reducer = (state = defaultState, action) => {
   // 由于 state 是引用类型，不能直接修改，否则是检测不到 state 发生变化的。因此需要先复制一份进行修改，然后再返回新的state
-  let newState = Object.assign({}, state)
+  // let newState = Object.assign({}, state)
   switch (action.type) {
     case constants.SET_DATA:
-      newState.myLoginData = action.data
-      return newState
+      // newState.myLoginData = action.data
+      // return newState
+      return getData(state, action)
     default:
       return state
   }
